0178edcf0520dc0e46cac9bf87da78e0684eb959
Simon Berger <simon-berger@hotmail.com>
2018-02-20 11:57:15 +0100

    Add separate token_iterator with forward iterator category

--- include/svbb/tokenize.hpp
+++ include/svbb/tokenize.hpp
@@ -1,95 +1,20 @@
 #pragma once
 #include "config.hpp"
-#include "split.hpp"
+#include "token_iterator.hpp"
 #include "trim.hpp"
 
 namespace SVBB_NAMESPACE {
-
-namespace detail {
-
-template<typename CharT, typename Traits, typename Splitter>
-class token_range_state
-{
-public:
-    using view_type = basic_string_view<CharT, Traits>;
-
-    SVBB_CONSTEXPR token_range_state(view_type input, Splitter splitter)
-        : data_(make_split(view_type(), input)), splitter_(splitter)
-    {
-    }
-
-    SVBB_CONSTEXPR view_type token() const SVBB_NOEXCEPT { return data_.left; }
-    SVBB_CONSTEXPR view_type remainder() const SVBB_NOEXCEPT { return data_.right; }
-    SVBB_CONSTEXPR bool empty() const SVBB_NOEXCEPT
-    {
-        return remainder().empty() && token().empty();
-    }
-    SVBB_CXX14_CONSTEXPR void split() { data_ = splitter_(remainder()); }
-
-private:
-    split_result<CharT, Traits> data_;
-    Splitter splitter_;
-};
-
-template<typename CharT, typename Traits, typename Splitter>
-class token_iterator
-{
-public:
-    using view_type = basic_string_view<CharT, Traits>;
-    using state_type = token_range_state<CharT, Traits, Splitter>;
-    using value_type = view_type;
-    using difference_type = std::ptrdiff_t;
-    using pointer = const view_type*;
-    using reference = view_type;
-    using iterator_category = std::input_iterator_tag;
-
-    SVBB_CONSTEXPR token_iterator() SVBB_NOEXCEPT : state_{nullptr} {};
-    SVBB_CXX14_CONSTEXPR token_iterator(state_type* data) : state_(data) { advance(); }
-
-    SVBB_CONSTEXPR reference operator*() const SVBB_NOEXCEPT { return state_->token(); }
-    SVBB_CXX14_CONSTEXPR token_iterator& operator++()
-    {
-        advance();
-        return *this;
-    }
-
-    SVBB_CXX14_CONSTEXPR token_iterator operator++(int)
-    {
-        token_iterator tmp = *this;
-        advance();
-        return tmp;
-    }
-
-    SVBB_CONSTEXPR bool operator==(const token_iterator& rhs) const SVBB_NOEXCEPT
-    {
-        return state_ == nullptr && rhs.state_ == nullptr;
-    }
-    SVBB_CONSTEXPR bool operator!=(const token_iterator& rhs) const SVBB_NOEXCEPT
-    {
-        return !(*this == rhs);
-    }
-
-private:
-    SVBB_CXX14_CONSTEXPR void advance()
-    {
-        state_->split();
-        if(state_->empty()) state_ = nullptr;
-    }
-
-    state_type* state_;
-};
 
 template<typename CharT, typename Traits, typename Splitter>
 class token_range
 {
 public:
-    using iterator = token_iterator<CharT, Traits, Splitter>;
+    using iterator = token_iterator_base<CharT, Traits, Splitter>;
     using const_iterator = iterator;
-    using state_type = typename iterator::state_type;
     using view_type = typename iterator::view_type;
 
     SVBB_CONSTEXPR token_range(view_type view, Splitter splitter)
-        : state_(view, splitter), begin_(&state_)
+        : state_(view, splitter), begin_(state_)
     {
     }
 
@@ -97,63 +22,73 @@ public:
     SVBB_CONSTEXPR auto end() const SVBB_NOEXCEPT -> iterator { return iterator(); }
 
 private:
+    using state_type = typename iterator::state;
+
     state_type state_;
     iterator begin_;
 };
 
 template<typename CharT>
-struct by_char
+class split_by_char
 {
-    SVBB_CONSTEXPR explicit by_char(CharT delimeter) : delimeter(delimeter) {}
+public:
+    SVBB_CONSTEXPR split_by_char() : delimeter_() {}
+    SVBB_CONSTEXPR explicit split_by_char(CharT delimeter) : delimeter_(delimeter) {}
 
     template<typename Traits>
     SVBB_CXX14_CONSTEXPR auto operator()(basic_string_view<CharT, Traits> input) const
         -> split_result<CharT, Traits>
     {
-        return split_around(input, std::min(input.find_first_of(delimeter), input.size()));
+        return split_around(input, std::min(input.find_first_of(delimeter_), input.size()));
     }
-    CharT delimeter;
+
+private:
+    CharT delimeter_;
 };
 
 template<typename CharT, typename Traits>
-struct trimmed_by_char
+class split_by_char_and_trim
 {
+public:
     using view_type = basic_string_view<CharT, Traits>;
-    SVBB_CONSTEXPR trimmed_by_char(CharT delimeter, view_type whitespace)
-        : whitespace(whitespace), delimeter(delimeter)
+    SVBB_CONSTEXPR split_by_char_and_trim() : whitespace_(), delimeter_() {}
+    SVBB_CONSTEXPR split_by_char_and_trim(CharT delimeter, view_type whitespace)
+        : whitespace_(whitespace), delimeter_(delimeter)
     {
     }
     SVBB_CXX14_CONSTEXPR auto operator()(view_type input) const -> split_result<CharT, Traits>
     {
-        input = trim_left(input, whitespace);
-        auto splitted = split_around(input, std::min(input.find_first_of(delimeter), input.size()));
-        splitted.left = trim_right(splitted.left, whitespace);
+        input = trim_left(input, whitespace_);
+        auto splitted =
+            split_around(input, std::min(input.find_first_of(delimeter_), input.size()));
+        splitted.left = trim_right(splitted.left, whitespace_);
         return splitted;
     }
-    view_type whitespace;
-    CharT delimeter;
+
+private:
+    view_type whitespace_;
+    CharT delimeter_;
 };
-} // namespace detail
 
 template<typename CharT, typename Traits, typename Splitter>
 SVBB_CXX14_CONSTEXPR auto tokenize(basic_string_view<CharT, Traits> view, Splitter splitter)
-    -> detail::token_range<CharT, Traits, Splitter>
+    -> token_range<CharT, Traits, Splitter>
 {
-    return detail::token_range<CharT, Traits, Splitter>(view, splitter);
+    return token_range<CharT, Traits, Splitter>(view, splitter);
 }
 
 template<typename CharT, typename Traits>
 SVBB_CXX14_CONSTEXPR auto tokenize(basic_string_view<CharT, Traits> view, CharT delimeter)
-    -> detail::token_range<CharT, Traits, detail::by_char<CharT>>
+    -> token_range<CharT, Traits, split_by_char<CharT>>
 {
-    return tokenize(view, detail::by_char<CharT>(delimeter));
+    return tokenize(view, split_by_char<CharT>(delimeter));
 }
 
 template<typename CharT, typename Traits>
 SVBB_CXX14_CONSTEXPR auto tokenize(basic_string_view<CharT, Traits> view, CharT delimeter,
                                    basic_string_view<CharT, Traits> whitespace)
-    -> detail::token_range<CharT, Traits, detail::trimmed_by_char<CharT, Traits>>
+    -> token_range<CharT, Traits, split_by_char_and_trim<CharT, Traits>>
 {
-    return tokenize(view, detail::trimmed_by_char<CharT, Traits>(delimeter, whitespace));
+    return tokenize(view, split_by_char_and_trim<CharT, Traits>(delimeter, whitespace));
 }
 } // namespace SVBB_NAMESPACE
---
+++ test/token_iterator.t.cpp
@@ -0,0 +1,42 @@
+#include "catch.hpp"
+#include "test_config.hpp"
+#include "svbb/token_iterator.hpp"
+#include "svbb/tokenize.hpp"
+#include "svbb/util.hpp"
+#include <vector>
+#include <algorithm>
+
+namespace {
+using namespace SVBB_NAMESPACE;
+
+TEST_CASE("equality")
+{
+    const auto first_input = make_view("a b c");
+    const auto second_input = make_view("a b c");
+    const auto third_input = make_view("c b a");
+    const auto splitter = split_by_char<char>(' ');
+
+    REQUIRE(begin_tokens(first_input, splitter) == begin_tokens(first_input, splitter));
+    REQUIRE(end_tokens(first_input, splitter) == end_tokens(first_input, splitter));
+    REQUIRE(begin_tokens(first_input, splitter) != end_tokens(first_input, splitter));
+
+    REQUIRE(begin_tokens(first_input, splitter) != begin_tokens(second_input, splitter));
+    REQUIRE(begin_tokens(first_input, splitter) != begin_tokens(third_input, splitter));
+    REQUIRE(begin_tokens(third_input, splitter) != begin_tokens(second_input, splitter));
+}
+
+TEST_CASE("copy")
+{
+    const auto first_input = make_view("a b c");
+    const auto second_input = make_view("a b c");
+    const auto third_input = make_view("c b a");
+    const auto splitter = split_by_char<char>(' ');
+
+    std::vector<string_view> tokens;
+    std::copy(begin_tokens(first_input, splitter), end_tokens(first_input, splitter),
+              std::back_inserter(tokens));
+
+    using Catch::Matchers::Equals;
+    REQUIRE_THAT(tokens, Equals(std::vector<string_view>{"a", "b", "c"}));
+}
+} // namespace
